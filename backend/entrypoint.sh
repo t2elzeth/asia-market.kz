e1c5bfe58dfe6a1c808ddd57fd5aa2e38a54fb67
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-02-16 22:07:42 +0600

    Update

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -4,6 +4,22 @@ python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-exec gunicorn -c "./gunicorn_config.py" core.wsgi
+echo "from os import getenv;
+from django.contrib.auth import get_user_model;
 
-python entrypoint.py
+User = get_user_model();
+
+USER_DEFAULT_USERNAMES = {'email': 'admin@gmail.com','username': 'admin','phone': '996312312312'};
+
+DEFAULT_USERNAME = USER_DEFAULT_USERNAMES.get(User.USERNAME_FIELD);
+if DEFAULT_USERNAME is None:raise ValueError('Unsupported USERNAME_FIELD');
+
+DEFAULT_PASSWORD = 'admin';
+
+USERNAME = getenv('DJANGO_ADMIN_USERNAME') or DEFAULT_USERNAME;
+PASSWORD = getenv('DJANGO_ADMIN_PASSWORD') or DEFAULT_PASSWORD;
+
+FILTER = {User.USERNAME_FIELD: USERNAME};
+if not User.objects.filter(**FILTER).exists():User.objects.create_superuser(USERNAME, PASSWORD);" | python manage.py shell
+
+exec gunicorn -c "./gunicorn_config.py" core.wsgi
