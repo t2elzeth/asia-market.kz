2801c9386dbce408153638ad92a05124b7be8741
Ulukman Amangeldiev <ulukmanatageldiuulu@gmail.com>
2021-03-04 13:26:07 +0600

    Update superuser creation

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -82,7 +82,6 @@ LOGGING = {
     },
 }
 
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,7 +3,6 @@
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
-
-echo "$(cat entrypoint.py.txt)" | python manage.py shell
+python manage.py initadmin
 
 exec gunicorn -c "./gunicorn_config.py" core.wsgi
